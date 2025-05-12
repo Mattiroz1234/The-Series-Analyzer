2f612d9a34870e0162ad422abc19dd9086b44c39
matti <m0556764778@gmail.com>
2025-05-12 23:55:33 +0300

    finised all the inpots function

--- The Series Analyzer/Program.cs
+++ The Series Analyzer/Program.cs
@@ -7,17 +7,64 @@ using System.Threading.Tasks;
 
 namespace The_Series_Analyzer
 {
-    internal class Program
+    public class Program
     {
+        static List<int> numbers = new List<int>();
+        static void InputRequest()
+        {
+            bool isStoper = true;
+
+            while (isStoper)
+            {
+                Console.WriteLine("enter positive number");
+                string input = Console.ReadLine();
+                if (input == "stop")
+                {
+                    if (numbers.Count >=3) 
+                    { isStoper = false;}
+                }
+                else
+                {
+                    if (IntCheck(input) && PosCheck(input))
+                        numbers.Add(int.Parse(input));
+                }
+            }
+        }
+
+        static bool IntCheck(string a)
+        {
+            int b;
+            if (int.TryParse(a,out b))
+                return true;
+            return false;
+        }
+
+        static bool PosCheck(string a)
+        {
+            if (int.Parse(a) > 0)
+                return true;
+            return false;
+        }
+
+        static void ProprietyCheck(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (IntCheck(arg) && PosCheck(arg))
+                    numbers.Add(int.Parse(arg));
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length < 3)
             {
-                //inputRequest()
+                InputRequest();
             }
             else
             {
-                //proprietyCheck(args)
+                ProprietyCheck(args);
             }
 
                 //menu(args)
